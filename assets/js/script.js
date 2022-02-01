2cc486d956b3667f3087ea980b06e96ba5985fa7
Edward Lopus <tedlopus@gmail.com>
2022-02-01 00:00:54 -0700

    Updated HTML to have timeblocks and added moment formats to create date and time in js

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var container = document.getElementsByClassName("container");
+
+// Use moment to display todays date
+var today = moment().format('MMMM Do YYYY');
+var todaysTime = moment().format("H A")
+
+
+// Create a table for the time blocks and make it so text can be written in each time block
+function createTimeblocks() {
+
+}
+
+// Write a function for you to save the text for each in local storage - maybe an event listener will work
+
+
+
+// Refreshing the page should not affect what it is saved in the planner
+
+
+createTimeblocks();
+
