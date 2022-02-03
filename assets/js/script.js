150f932622109c20a408c49515efadc8bedde43f
Edward Lopus <tedlopus@gmail.com>
2022-02-03 13:37:00 -0700

    Added timer and save button for textarea and formatted columsn for timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
-var container = document.getElementsByClassName("container");
+//var container = document.getElementsByClassName("container");
 
 // Use moment to display todays date
+var clock = setInterval(function(){
 var today = moment().format('MMMM Do YYYY');
-var todaysTime = moment().format("H A")
-
+var todaysTime = moment().format("H A");
+$("#currentDay").text(today + "/n " + todaysTime);
+},1000)
+//
 
 // Create a table for the time blocks and make it so text can be written in each time block
-function createTimeblocks() {
+//function createTimeblocks() {
 
-}
+//}
 
 // Write a function for you to save the text for each in local storage - maybe an event listener will work
 
@@ -17,5 +20,11 @@ function createTimeblocks() {
 // Refreshing the page should not affect what it is saved in the planner
 
 
-createTimeblocks();
+//createTimeblocks();
 
+$(".saveBtn").on("click", function(){
+    var userSave = $(this).siblings("textarea").val()
+    var timeBlock = $(this).parent().attr("id")
+    console.log(userSave, timeBlock)
+
+})
